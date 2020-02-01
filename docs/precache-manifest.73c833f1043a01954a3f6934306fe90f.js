ce5dfec1d880b81b0d3327b75ac305090a34ae7c
libertadcc <libertadchapinalcervantes@gmail.com>
2020-02-01 13:47:04 +0100

    Publish

---
+++ docs/precache-manifest.73c833f1043a01954a3f6934306fe90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed542862135800020553d03b34b7f288",
+    "url": "/identify/index.html"
+  },
+  {
+    "revision": "3317021c1f6001a5970d",
+    "url": "/identify/static/css/main.53226cc4.chunk.css"
+  },
+  {
+    "revision": "6624ad6768e9a275ad44",
+    "url": "/identify/static/js/2.36ceb3df.chunk.js"
+  },
+  {
+    "revision": "3317021c1f6001a5970d",
+    "url": "/identify/static/js/main.7283fa11.chunk.js"
+  },
+  {
+    "revision": "7a7f23fa991d6ca917ae",
+    "url": "/identify/static/js/runtime-main.4b69cf29.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.afd709517b36362f8c05d17f94d8e904.js"
+  "/identify/precache-manifest.73c833f1043a01954a3f6934306fe90f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/identify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
