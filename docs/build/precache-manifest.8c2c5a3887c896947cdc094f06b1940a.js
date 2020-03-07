a6c1738b4a982a519bbae57b70db0228160a5294
libertadcc <libertadchapinalcervantes@gmail.com>
2020-03-07 18:50:44 +0100

    Publish

---
+++ docs/build/precache-manifest.8c2c5a3887c896947cdc094f06b1940a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce3b9c60e73dedaa823d4108d42482c",
+    "url": "/identify/index.html"
+  },
+  {
+    "revision": "e6e499cc9fec10db5c30",
+    "url": "/identify/static/css/main.51851491.chunk.css"
+  },
+  {
+    "revision": "d159cde8e9694bfae1f2",
+    "url": "/identify/static/js/2.4ae6969d.chunk.js"
+  },
+  {
+    "revision": "e6e499cc9fec10db5c30",
+    "url": "/identify/static/js/main.88f59979.chunk.js"
+  },
+  {
+    "revision": "7a7f23fa991d6ca917ae",
+    "url": "/identify/static/js/runtime-main.4b69cf29.js"
+  }
+]);
