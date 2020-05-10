d33019a06d7004ac0caad6bee6b07d560170da72
libertadcc <libertadchapinalcervantes@gmail.com>
2020-05-10 21:06:52 +0200

    Login and sign up with material ui

--- src/views/LogIn.js
+++ src/views/LogIn.js
@@ -3,6 +3,29 @@ import { Link } from 'react-router-dom';
 import { withFirebaseHOC } from '../components/Firebase';
 import './login.scss';
 
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+
 class LogIn extends React.Component {
   constructor(){
     super();
@@ -10,13 +33,32 @@ class LogIn extends React.Component {
       emailLogin: '',
       passwordLogin: '',
       error: '',
+      showPassword: false,
+      saveUser: false,
     }
     this.changeEmailLogin = this.changeEmailLogin.bind(this);
     this.changePasswordLogin = this.changePasswordLogin.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
+    this.rememberUser = this.rememberUser.bind(this);
+    // this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
   }
 
+  rememberUser() { 
+    this.setState({saveUser: !this.state.saveUser});
+  }
+
+  handleClickShowPassword (){
+    console.log('this.state handleClickShowPassword', this.state.showPassword);
+    this.setState({showPassword: !this.state.showPassword})
+  };
+
+  handleMouseDownPassword(event) {
+    event.preventDefault();
+  };
+
   changeEmailLogin(event){
+    console.log('this.state', this.state)
     this.setState({emailLogin: event.currentTarget.value});
   }
 
@@ -27,7 +69,7 @@ class LogIn extends React.Component {
   async handleLogin() {
     const email = this.state.emailLogin;
     const password = this.state.passwordLogin;
-
+    
     try {
       const response = await this.props.firebase.loginWithEmail(
         email,
@@ -35,6 +77,8 @@ class LogIn extends React.Component {
       );
       console.log('r', response);
       if (response.user) {
+        if (this.state.saveUser) { localStorage.setItem('email', email) }
+        if (this.state.saveUser) { localStorage.setItem('password', password) }
         this.setState({error: ''})
         console.log("Usuario logado!!", response.user.uid);
         sessionStorage.setItem('userToken', response.user.uid);
@@ -48,24 +92,84 @@ class LogIn extends React.Component {
     }
   }
 
+  componentDidMount() {
+    if(localStorage.getItem('email')) {
+      const email = localStorage.getItem('email');
+      this.setState({emailLogin: email, saveUser: true});      
+    }
+    if(localStorage.getItem('password')) {
+      const password = localStorage.getItem('password');
+      this.setState({passwordLogin: password});
+    }
+  }
+
   render() {
     return (
+      <React.Fragment>
       <div className="container-login">
-        <h1 className="login-title">Iniciar sesión</h1>
-        <div className="login-form">
-          <label name="emailLogin">Email</label>
-          <input type="text" placeholder="Introduce el email" onChange={this.changeEmailLogin}/>
-          <label name="passwordLogin">Contraseña</label>
-          <input type="password" placeholder="Introduce la contraseña" onChange={this.changePasswordLogin}/>
-        </div>
+        <h1>Iniciar sesión</h1>
+        <form noValidate autoComplete="off" className="login-form">
+        <FormControl variant="outlined">
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={this.state.emailLogin}
+            onChange={this.changeEmailLogin}
+            className="input-login"
+          />
+          </FormControl>
+          <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Contraseña *</InputLabel>
+          <OutlinedInput
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Contraseña *"
+            type={this.state.showPassword ? 'text' : 'password'}
+            value={this.state.passwordLogin}
+            onChange={this.changePasswordLogin}
+            id="password"
+            autoComplete="current-password"
+            className="password-login"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                  >
+                  {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+          </FormControl>
+        </form>
+        <FormControlLabel
+            control={
+              <Checkbox 
+                value="remember"
+                className="btn-remember" 
+                onClick={this.rememberUser}
+                checked={this.state.saveUser}
+            />}
+            label="Recordar usuario"
+          />
         <p className={`error-message ${this.state.error === '' ? 'hidden' : 'shown'}`}>Credenciales incorrectas</p>
-        <button className="button-signin" onClick={this.handleLogin}>Iniciar sesión</button>
+        <Button className="button-signin" onClick={this.handleLogin} size="large">Iniciar sesión</Button>
         <Link to="/signup">
           <p className="link-signup">Registrarse</p>
         </Link>
       </div>
+      </React.Fragment>
     )
   }
 }
 
-export default withFirebaseHOC(LogIn)
+export default withFirebaseHOC(LogIn);
--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './login.scss';
 
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
 
 import { withFirebaseHOC } from '../components/Firebase'
 
 class SignUp extends React.Component {
-
   constructor(){
     super();
     this.state = {
@@ -15,12 +22,19 @@ class SignUp extends React.Component {
       errorEmailUsed: false,
       errorEmailFormat: false,
       errorPassword: false,
+      showPassword: false,
     }
     this.changeName = this.changeName.bind(this);
     this.changeEmail = this.changeEmail.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.signUp = this.signUp.bind(this);
+    this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
   }
+
+  handleClickShowPassword (){
+    console.log('this.state handleClickShowPassword', this.state.showPassword);
+    this.setState({showPassword: !this.state.showPassword})
+  };
 
   changeName(event){
     this.setState({name: event.currentTarget.value});
@@ -81,16 +95,73 @@ class SignUp extends React.Component {
         <div className="container-login">
           <h1 className="login-title">Nuevo usuario</h1>
           <div className="login-form">
-            <label name="name">Nombre</label>
-            <input type="text" placeholder="Nombre completo" onChange={this.changeName}/>
+            {/* <label name="name">Nombre</label>
+            <input type="text" placeholder="Nombre completo" onChange={this.changeName}/> */}
 
-            <label name="email">Email</label>
-            <input type="email" placeholder="Introduzca el email" onChange={this.changeEmail}/>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="nombre"
+              label="Nombre"
+              name="name"
+              autoFocus
+              placeholder="Nombre completo *"
+              value={this.state.emailLogin}
+              onChange={this.changeName}
+              className="input-login"
+              type="text"
+            />
+
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              placeholder="Introduzca el email"
+              value={this.state.emailLogin}
+              onChange={this.changeEmail}
+              className="input-login"
+              type="email"
+            />
+
+            <FormControl variant="outlined" className="input-login">
+              <InputLabel htmlFor="outlined-adornment-password">Contraseña *</InputLabel>
+              <OutlinedInput
+                //className="input-login"
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Contraseña *"
+                type={this.state.showPassword ? 'text' : 'password'}
+                value={this.state.password}
+                onChange={this.changePassword}
+                id="password"
+                autoComplete="current-password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={this.handleClickShowPassword}
+                      >
+                      {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+          </FormControl>
+            {/* <label name="email">Email</label>
+            <input type="email" placeholder="Introduzca el email" onChange={this.changeEmail}/> */}
             
-            <label name="password">Contraseña</label>
-            <input type="password" placeholder="Introduzca una contraseña" onChange={this.changePassword}/>
+            {/* <label name="password">Contraseña</label>
+            <input type="password" placeholder="Introduzca una contraseña" onChange={this.changePassword}/> */}
           </div>
-          <button className="button-signin" onClick={this.signUp}>Registrarse</button>
+          <button className="button-signin btn-signup" onClick={this.signUp}>Registrarse</button>
         </div>
         {this.state.errorPassword === true? <p className="error-message">La constraseña debe tener, al menos, 6 caracteres.</p> : null }
         {this.state.errorEmailUsed ? <p className="error-message">Ese email no está disponible.</p> : null }
